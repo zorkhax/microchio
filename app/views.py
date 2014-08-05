335285d1325cb9ddf5bd4ade98866df100ae66db
zorkhax <zorkhax@gmail.com>
2014-08-05 16:03:13 +0700

    Screenshots added, post template changed.

--- app/views.py
+++ app/views.py
@@ -176,21 +176,6 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-# @app.route('/user/<nickname>')
-# @login_required
-# def user(nickname):
-#     user = User.query.filter_by(nickname = nickname).first()
-#     if user == None:
-#         flash('User ' + nickname + ' not found.')
-#         return redirect(url_for('index'))
-#     posts = [
-#         { 'author': user, 'body': 'Test post #1' },
-#         { 'author': user, 'body': 'Test post #2' }
-#     ]
-#     return render_template('user.html',
-#         user = user,
-#         posts = posts)
-
 @app.route('/userid/<int:id>')
 @login_required
 def userid(id):
@@ -202,7 +187,6 @@ def userid(id):
     return render_template('user.html',
         user = user,
         posts = posts)
-
 
 @app.route('/edit', methods = ['GET', 'POST'])
 @login_required
